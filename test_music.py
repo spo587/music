a9390e06d3b4efe7184d4fbfaac0be900b51daa7
spo587 <samueljpost@gmail.com>
2013-07-03 14:58:53 -0400

    deleted unused functions, all in classes now

--- test_music.py
+++ test_music.py
@@ -51,31 +51,6 @@ def play_tune(half_steps_above_440_and_length_list):
     return result
 
 
-def play_waveform((length, freq)):
-    s = 'play -n synth %s sine %s' % (length, freq)
-    #s = 'echo %s | sox -r 60000 -b 8 -c 1 -t raw -s - -d' %form
-    #print s
-    return os.system(s)
- 
-    #p = subprocess.Popen(['sox', '-r', '8000', '-b', '8', '-c', '1', '-t', 'raw', '-s', '-', '-d'], stdin=subprocess.PIPE)
-    #p.stdin.write(form)
-    
- 
-def make_note(rate, half_steps_above_400, length=1):
-    cycles = int(rate / (400 * 2**(float(half_steps_above_400)/12)))
-    cycle = cycles/2 * 'a' + cycles/2 * 'r'
-    return int(rate / len(cycle) * length) * cycle
-
-
-def length(note_value):
-    '''tempo in beats per minute'''
-    return note_value*4*60/float(tempo)
-
-def make_note_simpler((half_steps_above_440, note_value)):
-    freq = 440*2**(float(half_steps_above_440)/12)
-    return (length(note_value),freq)
- 
- 
 
         
  
@@ -85,9 +60,6 @@ def make_note_simpler((half_steps_above_440, note_value)):
 if __name__ == '__main__':
     tempo = 300
     note1.play_note()
-    #play_waveform(make_note(g_rate, 0, .5) + make_note(g_rate, 0, .5) +  make_note(g_rate, 1, .5) + make_note(g_rate, 3, .5))
-    play_tune([(0,0.5),(0,0.5),(1,0.5),(3,0.5),(3,0.5),(1,0.5),(0,0.5),(-2,0.5),(-4,0.5),(-4,0.5),(-2,0.5),(0,0.5),(0,0.75),(-2,0.25),(-2,0.5)])
-    #play_waveform(make_note_simpler(0,0.01)) #+ play_waveform(make_note_simpler(0)) + play_waveform(make_note_simpler(1)) + play_waveform(make_note_simpler(3))
-
+    
     #for i in range(-2,4):
     #    play_waveform(make_note(g_rate,i,.5))
