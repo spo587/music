a1a779feab2a1785836aa6e00f019e3f509ce7be
spo587 <samueljpost@gmail.com>
2013-07-03 14:58:10 -0400

    added tune class, now everything is there. this version has old functions for safekeeping

--- test_music.py
+++ test_music.py
@@ -1,21 +1,48 @@
 import subprocess
 import os
 
+
 #g_rate = 60000 
 class Note(object):
+    tempo = 200
     note_dict = {'a':0,'b':2,'c':3,'d':5,'e':7,'f':8,'g':10}
     for note in note_dict.keys():
-        note_dict[note + '+'] = note_dict[note] + 1
-        note_dict[note + '-'] = note_dict[note] - 1
+        note_dict[note + '#'] = note_dict[note] + 1
+        note_dict[note + 'b'] = note_dict[note] - 1
         #doublesharps and flats too
 
     def __init__(self, name, value):
         '''name a tuple of (note name, octave)'''
         self.name = name
         self.value = value
+
+    def parse_length(self):
         
+        length = self.value*4*60/float(tempo)
+        return length
+    def parse_freq(self):
+        half_steps_above_440 = Note.note_dict[self.name[0]]+12*self.name[1]
+        freq = 440*2**(float(half_steps_above_440)/12)
+        return freq
+
+        
+    def play_note(self):
+        s = 'play -n synth %s sine %s' % (self.parse_length(), self.parse_freq())
+        return os.system(s)
+
+class Tune(object):
+    def __init__(self,notes):
+        self.notes = notes
     def play(self):
-        n = make_note_simpler((note_dict[self.name[0]]*12*self.name[1],self.value))
+        result = notes[0].play_note()
+        for i in range(1,len(self.notes)):
+            result += notes[i].play_note()
+        return result
+
+ode_firstphrase = Tune([Note(('f',-2),1/2.0),Note(('a',0),1/4.0),Note(('bb',0),1/4.0),Note(('a',0),1/4.0)])
+note1 = Note(('b#',0),2.0)
+
+
 
 def play_tune(half_steps_above_440_and_length_list):
     result = play_waveform(make_note_simpler(half_steps_above_440_and_length_list[0]))
@@ -57,6 +84,7 @@ def make_note_simpler((half_steps_above_440, note_value)):
  
 if __name__ == '__main__':
     tempo = 300
+    note1.play_note()
     #play_waveform(make_note(g_rate, 0, .5) + make_note(g_rate, 0, .5) +  make_note(g_rate, 1, .5) + make_note(g_rate, 3, .5))
     play_tune([(0,0.5),(0,0.5),(1,0.5),(3,0.5),(3,0.5),(1,0.5),(0,0.5),(-2,0.5),(-4,0.5),(-4,0.5),(-2,0.5),(0,0.5),(0,0.75),(-2,0.25),(-2,0.5)])
     #play_waveform(make_note_simpler(0,0.01)) #+ play_waveform(make_note_simpler(0)) + play_waveform(make_note_simpler(1)) + play_waveform(make_note_simpler(3))
