0e1c54bad3763c8e8ba00659c0abeceba763949f
spo587 <samueljpost@gmail.com>
2013-07-07 15:20:36 -0400

    added clarifying messages

--- make_sine_wave.py
+++ make_sine_wave.py
@@ -2,6 +2,9 @@ import subprocess
 import os
 import math
 note_string = ''
+
+## building a sine wave out of characters. change the length of the step to change the frequency of the wave. current 
+## current frequency is 416 Hz.
 for i in range(33,127,8):
     note_string += chr(int(round(126*math.sin(math.pi*i/(2*126)))))
 
@@ -12,14 +15,16 @@ for i in range(len(note_string_reversed)):
     note_string += (note_string_reversed[i])
 
 
+
 note_string *= 1000
+## print to a new file or comment out
 print note_string
 
 
 
 
+## this part doesn't work, throws weird errors at me
 
 # note = 'echo %s | sox -r 50000 -b 8 -c 1 -t raw -s - -d' %note_string
-# # # # # #print s
 # os.system(note)
 
