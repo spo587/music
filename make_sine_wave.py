9c250d12aa7dda2e644333668f7c77c86bc513fb
spo587 <samueljpost@gmail.com>
2013-07-07 15:14:09 -0400

    many changes

---
+++ make_sine_wave.py
@@ -0,0 +1,23 @@
+import subprocess
+import os
+import math
+note_string = ''
+for i in range(33,127,8):
+    note_string += chr(int(round(126*math.sin(math.pi*i/(2*126)))))
+
+
+note_string_reversed = list(note_string)
+note_string_reversed.reverse()
+for i in range(len(note_string_reversed)):
+    note_string += (note_string_reversed[i])
+
+
+note_string *= 1000
+print note_string
+
+
+
+# note = 'echo %s | sox -r 50000 -b 8 -c 1 -t raw -s - -d' %note_string
+# # # # # #print s
+# os.system(note)
+
