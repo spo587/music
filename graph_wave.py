187031a7a7186bea61f082fb153b075171421e27
spo587 <samueljpost@gmail.com>
2013-07-08 11:06:54 -0400

    trying to graph output file, added name=main to main make sine wave file

---
+++ graph_wave.py
@@ -0,0 +1,5 @@
+
+
+s = open(make_sine_wave).read()
+
+graph([ord(c) for c in s])
--- make_sine_wave.py
+++ make_sine_wave.py
@@ -5,16 +5,19 @@ note_string = ''
 
 ## building a sine wave out of characters. change the length of the step to change the frequency of the wave. current 
 
-freq = 600.0
-sample_rate = 50000.0
+freq = 300.0
+sample_rate = 20000.0
 
-for i in range(0,int(sample_rate/freq)+1):
+for i in range(0,int(sample_rate/freq)):
     note_string+= chr(int(round(255/2.0*math.sin(math.pi*2*i*freq/sample_rate)+255/2.0)))
 
 
 note_string *= 1000
 
-p = subprocess.Popen(['sox', '-r', '50000', '-b', '8', '-c', '1', '-t', 'raw', '-s', '-', '-d'], stdin=subprocess.PIPE)
-p.stdin.write(note_string)
+if __name__ == '__main__':
+
+
+    p = subprocess.Popen(['sox', '-r', '20000', '-b', '8', '-c', '1', '-t', 'raw', '-s', '-', '-d'], stdin=subprocess.PIPE)
+    p.stdin.write(note_string)
 
 
