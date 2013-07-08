7958f198a0f49b909b8390e1ec131832448a056c
spo587 <samueljpost@gmail.com>
2013-07-07 22:05:23 -0400

    modified to use stdin and real sine wave

--- make_sine_wave.py
+++ make_sine_wave.py
@@ -4,27 +4,15 @@ import math
 note_string = ''
 
 ## building a sine wave out of characters. change the length of the step to change the frequency of the wave. current 
-## current frequency is 416 Hz.
-for i in range(33,127,8):
-    note_string += chr(int(round(126*math.sin(math.pi*i/(2*126)))))
 
 
-note_string_reversed = list(note_string)
-note_string_reversed.reverse()
-for i in range(len(note_string_reversed)):
-    note_string += (note_string_reversed[i])
-
+for i in range(0,505,4):
+    note_string += chr(int(round(0.3*126*math.sin(math.pi*i/(2*126))))+80)
 
 
 note_string *= 1000
-## print to a new file or comment out
-print note_string
 
+p = subprocess.Popen(['sox', '-r', '50000', '-b', '8', '-c', '1', '-t', 'raw', '-s', '-', '-d'], stdin=subprocess.PIPE)
+p.stdin.write(note_string)
 
-
-
-## this part doesn't work, throws weird errors at me
-
-# note = 'echo %s | sox -r 50000 -b 8 -c 1 -t raw -s - -d' %note_string
-# os.system(note)
 
