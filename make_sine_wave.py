5b6dbac574e08913f5418f6555a8a4237852dfd7
spo587 <samueljpost@gmail.com>
2013-07-08 00:39:42 -0400

    deleted old stuff

--- make_sine_wave.py
+++ make_sine_wave.py
@@ -5,10 +5,6 @@ note_string = ''
 
 ## building a sine wave out of characters. change the length of the step to change the frequency of the wave. current 
 
-
-# for i in range(0,505,4):
-#     note_string += chr(int(round(0.3*126*math.sin(math.pi*i/(2*126))))+80)
-
 freq = 600.0
 sample_rate = 50000.0
 
