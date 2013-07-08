ab421da0892b1ea70f96206dc652ac570531b732
spo587 <samueljpost@gmail.com>
2013-07-08 18:30:27 -0400

    converted length input parameter to duration in seconds

--- make_sine_wave.py
+++ make_sine_wave.py
@@ -9,30 +9,29 @@ bit = 32
 sample_rate = 100000.0
 
  
-def make_sine_wave(freq,length):
+def make_sine_wave(freq,duration):
     note_string_ints = []
-    for i in range(0,int(length*sample_rate/freq)):
+    for i in range(0,int(duration*freq*sample_rate/freq)):
         amplitude = (2**bit - 1)/2.0
         offset = (2**bit - 1)/2.0
         phase = math.pi*2*i*freq/sample_rate
         note_string_ints.append(int(round(amplitude*math.sin(phase)+offset)))
     return note_string_ints
 
-y = make_sine_wave(440,10)
-x = range(len(y))
 
-mpl.scatter(x,y)
-mpl.show()
-# var1 = make_sine_wave(440)
-# y = var1
-# x = range(len(var1))
-# mpl.scatter(x,y)
-# mpl.show()
 
 def make_bytes_wave(note_string_ints):
     l = [struct.pack('I',num) for num in note_string_ints]
     bytes  = ''.join(l)
     return bytes
+
+
+
+def make_sound(freq,duration):
+    ints = make_sine_wave(freq,duration)
+    bytes = make_bytes_wave(ints)
+    return bytes
+
 
 def bytes_to_ints(bytes):
     l =[]
@@ -43,20 +42,12 @@ def bytes_to_ints(bytes):
     return l
 
 
-def make_sound(freq,length):
-    ints = make_sine_wave(freq,length)
-    bytes = make_bytes_wave(ints)
-    return bytes
-
-
-
-
 if __name__ == '__main__':
 
 
     p = subprocess.Popen(['sox', '-r', str(sample_rate), '-b', str(bit) , '-c', '1', '-t', 'raw', '-e', 'unsigned-integer', '-', '-d'], stdin=subprocess.PIPE)
 
-    p.stdin.write(make_sound(455,1000))
+    p.stdin.write(make_sound(500,2))
 
 
 
