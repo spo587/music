6e491210be5faba6bfb47fd20c3c397b9108a0a5
spo587 <samueljpost@gmail.com>
2013-07-09 00:09:33 -0400

    added function for tone of two sine waves

--- make_sine_wave.py
+++ make_sine_wave.py
@@ -25,12 +25,21 @@ def make_bytes_wave(note_string_ints):
     bytes  = ''.join(l)
     return bytes
 
+def combine_two_sine_waves(wave1, wave2):
+    note_string_ints = [(x+y)/2.0 for x,y in zip(wave1,wave2)]
+    return note_string_ints
 
 
 def make_sine_wave_sound(freq,duration):
     ints = build_sine_wave(freq,duration)
     bytes = make_bytes_wave(ints)
     return bytes
+
+def make_tone(freq1,freq2,duration,):
+    ints = combine_two_sine_waves(build_sine_wave(freq1,duration),build_sine_wave(freq2,duration))
+    bytes = make_bytes_wave(ints)
+    return bytes
+
 
 
 def bytes_to_ints(bytes):
@@ -47,7 +56,7 @@ if __name__ == '__main__':
 
     p = subprocess.Popen(['sox', '-r', str(sample_rate), '-b', str(bit) , '-c', '1', '-t', 'raw', '-e', 'unsigned-integer', '-', '-d'], stdin=subprocess.PIPE)
 
-    p.stdin.write(make_sound(500,2))
+    p.stdin.write(make_tone(500,1000,2))
 
 
 
