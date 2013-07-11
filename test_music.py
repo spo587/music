59ae9852b0723cbf001c34227eec36e07dde3188
spo587 <samueljpost@gmail.com>
2013-07-11 10:08:35 -0400

    changed tunes, modified tone class so that over here writing out tunes is a little more compact

--- test_music.py
+++ test_music.py
@@ -24,25 +24,25 @@ silence = msw.Instrument('silent',dict3,dict4,dict2)
  
 def twinkle_twinkle():
 
-    a = msw.Tone(('a',0),1.0)
-    e = msw.Tone(('e',0),1.0)
-    fsharp = msw.Tone(('f#',0),1.0)
-    d = msw.Tone(('d',0),1.0)
-    csharp = msw.Tone(('c#',0),1.0)
-    csharp_16th = msw.Tone(('c#',0),1/4.0) 
-    b = msw.Tone(('b',0),3/4.0)
-    a_whole = msw.Tone(('a',0),1.0/2.0)
-    bass_a = msw.Tone(('a',-2),1.0)
-    middle_a = msw.Tone(('a',-1),1.0)
-    bass_csharp = msw.Tone(('c#',-1),1.0)
-    middle_d = msw.Tone(('d',-1),1.0)
-    bass_b = msw.Tone(('b',-1),1.0)
-    bass_gsharp = msw.Tone(('g#',-2),1.0)
-    bass_fsharp = msw.Tone(('f#',-2),1.0)
-    bass_d = msw.Tone(('d',-2),1.0)
-    bass_e = msw.Tone(('e',-2),1.0)
-    bass_a_whole = msw.Tone(('a',-2),2.0)
-    a_16th = msw.Tone(('a',0),1/16.0)
+    a = msw.Tone('a',0,1.0)
+    e = msw.Tone('e',0,1.0)
+    fsharp = msw.Tone('f#',0,1.0)
+    d = msw.Tone('d',0,1.0)
+    csharp = msw.Tone('c#',0,1.0)
+    csharp_16th = msw.Tone('c#',0,1/4.0) 
+    b = msw.Tone('b',0,3/4.0)
+    a_whole = msw.Tone('a',0,1.0/2.0)
+    bass_a = msw.Tone('a',-2,1.0)
+    middle_a = msw.Tone('a',-1,1.0)
+    bass_csharp = msw.Tone('c#',-1,1.0)
+    middle_d = msw.Tone('d',-1,1.0)
+    bass_b = msw.Tone('b',-1,1.0)
+    bass_gsharp = msw.Tone('g#',-2,1.0)
+    bass_fsharp = msw.Tone('f#',-2,1.0)
+    bass_d = msw.Tone('d',-2,1.0)
+    bass_e = msw.Tone('e',-2,1.0)
+    bass_a_whole = msw.Tone('a',-2,2.0)
+    a_16th = msw.Tone('a',0,1/16.0)
 
     melody = [a,a,e,e,fsharp,fsharp,e,e,
               d,d,csharp,csharp,b,a_16th,b,csharp_16th,a_whole]
@@ -62,29 +62,29 @@ def twinkle_twinkle():
     return chords
 
 def somewhere1():
-    one = msw.Tone(('a',-1),2.0,crappy_instrument)
-    two = msw.Tone(('a',-0),2.0,crappy_instrument)
-    three = msw.Tone(('g#',-1),1.0,crappy_instrument)
-    four = msw.Tone(('e',-1),1/2.0,crappy_instrument)
-    five = msw.Tone(('f#',-1),1/2.0,crappy_instrument)
+    one = msw.Tone('a',-1,2.0,crappy_instrument)
+    two = msw.Tone('a',-0,2.0,crappy_instrument)
+    three = msw.Tone('g#',-1,1.0,crappy_instrument)
+    four = msw.Tone('e',-1,1/2.0,crappy_instrument)
+    five = msw.Tone('f#',-1,1/2.0,crappy_instrument)
     six = three
-    seven = msw.Tone(('a',-0),1.0,crappy_instrument)
-    eight = msw.Tone(('f#',-2),2.0,crappy_instrument)
-    nine = msw.Tone(('f#',-1),2.0,crappy_instrument)
-    ten = msw.Tone(('e',-1),1.0,crappy_instrument)
-    offset_bass = msw.Tone('rest',2.0,crappy_instrument)
+    seven = msw.Tone('a',-0,1.0,crappy_instrument)
+    eight = msw.Tone('f#',-2,2.0,crappy_instrument)
+    nine = msw.Tone('f#',-1,2.0,crappy_instrument)
+    ten = msw.Tone('e',-1,1.0,crappy_instrument)
+    offset_bass = msw.Tone('rest',None,2.0,crappy_instrument)
 
-    offset = msw.Tone('rest',2.0,crappy_instrument)
-    one_bass = msw.Tone(('a',0),2.0,crappy_instrument)
-    two_bass = msw.Tone(('a',1),2.0,crappy_instrument)
-    three_bass = msw.Tone(('g#',0),1.0,crappy_instrument)
-    four_bass = msw.Tone(('e',0),1/2.0,crappy_instrument)
-    five_bass = msw.Tone(('f#',0),1/2.0,crappy_instrument)
+    offset = msw.Tone('rest',None,2.0,crappy_instrument)
+    one_bass = msw.Tone('a',0,2.0,crappy_instrument)
+    two_bass = msw.Tone('a',1,2.0,crappy_instrument)
+    three_bass = msw.Tone('g#',0,1.0,crappy_instrument)
+    four_bass = msw.Tone('e',0,1/2.0,crappy_instrument)
+    five_bass = msw.Tone('f#',0,1/2.0,crappy_instrument)
     six_bass = three_bass
-    seven_bass = msw.Tone(('a',1),1.0,crappy_instrument)
-    eight_bass = msw.Tone(('f#',-1),2.0,crappy_instrument)
-    nine_bass = msw.Tone(('f#',0),2.0,crappy_instrument)
-    ten_bass = msw.Tone(('e',0),1.0,crappy_instrument)
+    seven_bass = msw.Tone('a',1,1.0,crappy_instrument)
+    eight_bass = msw.Tone('f#',-1,2.0,crappy_instrument)
+    nine_bass = msw.Tone('f#',0,2.0,crappy_instrument)
+    ten_bass = msw.Tone('e',0,1.0,crappy_instrument)
     melody_bass = [one,two,three,four,five,six,seven,eight,nine,ten,offset_bass]
     melody_treble = [offset,one_bass,two_bass,three_bass,four_bass,five_bass,six_bass,seven_bass,eight_bass,nine_bass,ten_bass]
 
@@ -96,16 +96,16 @@ def somewhere1():
 
 def bach_herzliebster():
 
-    soprano = msw.Line([[('a',0),1.0],[('a',-0),1.0],[('a',-0),1.0],[('g#',-1),1/2.0],[('f#',-1),1/2.0],
-                        [('e',-1),1.0]],crappy_instrument) 
+    soprano = msw.Line([['a',0,1.0],['a',-0,1.0],['a',-0,1.0],['g#',-1,1/2.0],['f#',-1,1/2.0],
+                        ['e',-1,1.0]],crappy_instrument) 
 
-    alto = msw.Line([[('e',-1),1.0],[('f',-1),1.0],[('f',-1),1.0],[('e',-1),1.0],[('b',-1),1.0]],crappy_instrument)
+    alto = msw.Line([['e',-1,1.0],['f',-1,1.0],['f',-1,1.0],['e',-1,1.0],['b',-1,1.0]],crappy_instrument)
     
-    tenor = msw.Line([[('c',-1),1.0],[('d',-1),1.0],[('c',-1),1.0],[('b',-1),1/2.0],[('a',-1),1/2.0],
-                    [('g#',-2),1.0]],crappy_instrument)
+    tenor = msw.Line([['c',-1,1.0],['d',-1,1.0],['c',-1,1.0],['b',-1,1/2.0],['a',-1,1/2.0],
+                    ['g#',-2,1.0]],crappy_instrument)
 
-    bass = msw.Line([[('a',-1),1.0],[('d',-2),1/2.0],[('e',-2),1/2.0],[('f',-2),1/2.0],[('d',-2),1/2.0],
-                    [('e',-2),3/2.0],[('d',-2),1/2.0]],crappy_instrument)
+    bass = msw.Line([['a',-1,1.0],['d',-2,1/2.0],['e',-2,1/2.0],['f',-2,1/2.0],['d',-2,1/2.0],
+                    ['e',-2,3/2.0],['d',-2,1/2.0]],crappy_instrument)
 
     lines = [soprano,alto,tenor,bass]
     lines_ready = [msw.combine_things_ints(line.make_tones()) for line in lines]
@@ -114,23 +114,23 @@ def bach_herzliebster():
     return chords
 
 def somewhere2():
-    line1 = msw.Line([[('eb',-1),2.0],[('eb',0),2.0],[('d',0),1.0],[('bb',0),1/2.0],[('c',0),1/2.0],[('d',0),1.0],
-                    [('eb',0),1.0],[('eb',-1),2.0],[('c',0),2.0],[('bb',0),4.0],
-                    [('c',-1),2.0],[('ab',-0),2.0],[('g',-1),1.0],[('eb',-1),1/2.0],[('f',-1),1/2.0],
-                    [('g',-1),1.0],[('ab',-0),1.0],[('f',-1),1.0],[('d',-1),0.5],[('eb',-1),0.5],[('f',-1),1.0],
-                    [('g',-1),1.0],[('eb',-1),4.0]],crappy_instrument)
-    line2 = msw.Line([[('bb',-1),2.0],[('bb',0),2.0],[('f',-1),3.0],[('g',-1),1.0],[('eb',-1),4.0],[('eb',-1),4.0],
-                    [('c',-1),3.0],[('d',-1),0.5],[('c',-1),0.5],[('b',-1),2.0],[('c',-1),2.0],[('rest'),1.5],[('eb',-1),1.5],
-                    [('d',-1),1.0],[('eb',-1),4.0]],crappy_instrument)
-    line3 = msw.Line([[('g',-2),2.0],[('g',-1),2.0],[('rest'),4.0],[('c',-1),1.0],[('ab',-1),0.5],[('bb',-1),0.5],
-                    [('c',-1),1.0],[('d',-1),1.0],[('eb',-1),4.0],[('rest'),6.0],[('bb',-1),2.0],[('c',-1),3.0],
-                    [('bb',-1),1.0],[('g',-2),4.0]],crappy_instrument)
+    line1 = msw.Line([['eb',-1,2.0],['eb',0,2.0],['d',0,1.0],['bb',0,1/2.0],['c',0,1/2.0],['d',0,1.0],
+                    ['eb',0,1.0],['eb',-1,2.0],['c',0,2.0],['bb',0,4.0],
+                    ['c',-1,2.0],['ab',-0,2.0],['g',-1,1.0],['eb',-1,1/2.0],['f',-1,1/2.0],
+                    ['g',-1,1.0],['ab',-0,1.0],['f',-1,1.0],['d',-1,0.5],['eb',-1,0.5],['f',-1,1.0],
+                    ['g',-1,1.0],['eb',-1,4.0]],crappy_instrument)
+    line2 = msw.Line([['bb',-1,2.0],['bb',0,2.0],['f',-1,3.0],['g',-1,1.0],['eb',-1,4.0],['eb',-1,4.0],
+                    ['c',-1,3.0],['d',-1,0.5],['c',-1,0.5],['b',-1,2.0],['c',-1,2.0],['rest',None,1.5],['eb',-1,1.5],
+                    ['d',-1,1.0],['eb',-1,4.0]],crappy_instrument)
+    line3 = msw.Line([['g',-2,2.0],['g',-1,2.0],['rest',None,4.0],['c',-1,1.0],['ab',-1,0.5],['bb',-1,0.5],
+                    ['c',-1,1.0],['d',-1,1.0],['eb',-1,4.0],['rest',None,6.0],['bb',-1,2.0],['c',-1,3.0],
+                    ['bb',-1,1.0],['g',-2,4.0]],crappy_instrument)
 
-    line4 = msw.Line([[('rest'),4.0],[('bb',-2),2.0],[('bb',-1),2.0],[('rest'),1.0],[('ab',-1),3.0],
-                    [('g',-2),1.0],[('eb',-2),0.5],[('f',-2),0.5],[('g',-2),1.0],[('ab',-1),1/2.0],[('bb',-1),0.5],
-                    [('ab',-1),2.0],[('f',-3),2.0],[('g',-3),1.0],[('f',-2),1.0],[('e',-2),1.0],
-                    [('f',-2),0.5],[('g',-2),0.5],[('ab',-1),1.5],[('g',-2),0.5],[('ab',-1),0.5],
-                    [('a',-1),0.5],[('bb',-1),1.0],[('eb',-2),4.0]],crappy_instrument)
+    line4 = msw.Line([['rest',None, 4.0],['bb',-2,2.0],['bb',-1,2.0],['rest',None,1.0],['ab',-1,3.0],
+                    ['g',-2,1.0],['eb',-2,0.5],['f',-2,0.5],['g',-2,1.0],['ab',-1,1/2.0],['bb',-1,0.5],
+                    ['ab',-1,2.0],['f',-3,2.0],['g',-3,1.0],['f',-2,1.0],['e',-2,1.0],
+                    ['f',-2,0.5],['g',-2,0.5],['ab',-1,1.5],['g',-2,0.5],['ab',-1,0.5],
+                    ['a',-1,0.5],['bb',-1,1.0],['eb',-2,4.0]],crappy_instrument)
 
     lines = [line1,line2,line3,line4]
 
@@ -154,7 +154,7 @@ def somewhere3():
 if __name__ == '__main__':
     t_0 = time.time()
     #tune1 = bach_herzliebster()
-    tune1 = somewhere3()
+    tune1 = somewhere2()
     t_1 = time.time()
     print t_1 - t_0
     msw.play2(tune1)
