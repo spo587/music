77544258c36e10611cc1a9cbee55179dca0385f2
spo587 <samueljpost@gmail.com>
2013-07-10 23:26:41 -0400

    completed somewhere first phrase and made rests more clear and started new line class but not implemented yet

--- make_sine_wave.py
+++ make_sine_wave.py
@@ -5,21 +5,10 @@ import struct
 from collections import defaultdict
 import time
 
-
 bit = 32
-sample_rate = 20000.0
+sample_rate = 10000.0
 wave_peak = (2**bit - 1)/2.0
-tempo = 80
-
-def exponential_decay(k,time):
-    return math.exp(-k*time)
-
-def linear_decay(k,time):
-    return (1 - k*time)
-
-def no_decay(k,time):
-    return 1
-
+tempo = 180
 
 class Instrument(object):
     def __init__(self,name,overtone_decay_func_dict,overtone_strengths_dict,overtone_constants_dict):
@@ -45,25 +34,6 @@ class Instrument(object):
         bytes = convert_to_bytes(note_string_ints)
         return bytes
 
-
-
-dict1 = {1:0.5, 2: 0.1, 3: 0.1, 4: 0.05, 5: 0.05, 6: 0.01, 7:0.01, 8: 0.01, 9: 0.01}
-dict2 = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.2, 5: 7.0, 6: 12.0, 7: 12.0, 8:7.0, 9: 8.0}
-dict3 = {}
-for i in range(1,5):
-    dict3[i] = linear_decay
-for i in range(5,14):
-    dict3[i] = exponential_decay
-
-crappy_instrument = Instrument('crappy',dict3,dict1,dict2)
-
-dict4 = {}
-for key in dict1.keys():
-    dict4[key]=0
-
-
-silence = Instrument('silent',dict3,dict4,dict2)
-
 class Line(object):
     def __init__(self,notes_value_pairs,instrument):
         '''notes_value_pairs will be a list each element has form [('a',-1),1/4.0]'''
@@ -73,8 +43,35 @@ class Line(object):
     def make_tones(self):
         tones = []
         for notes_value_pair in self.notes_value_pairs:
-            tones.append(Tone(notes_value_pair[0],notes_value_pair[1]))
+            tones.append(Tone(notes_value_pair[0],notes_value_pair[1],self.instrument))
         return tones
+
+    def transpose_tones(self,num_halfsteps):
+        l = self.make_tones()
+        for tone in l:
+            tone.fund_freq *= (2**num_halfsteps/12.0)
+        return l
+
+# class Line_steps(object):
+#     def __init__(self,initial_note,steps_values,instrument):
+#         '''initial note has form [('a',-1),1/4.0],each element of steps_values has form [4,1.0], first element
+#         half-steps, second element number of beats'''
+#         self.initial_note = initial_note
+#         self.instrument = instrument
+#         self.steps_values = steps_values
+
+#     def make_tones(self):
+#         tones = [Tone(initial_note[0],initial_note[1],self.instrument)]
+#         for step_value in self.steps_values:
+#             tones.append(Tone(step_value[0],notes_value_pair[1],self.instrument))
+#         return tones
+
+#     def transpose_tones(self,num_halfsteps):
+#         l = self.make_tones()
+#         for tone in l:
+#             tone.fund_freq *= (2**num_halfsteps/12.0)
+#         return l
+
 
 class Chord(object):
     def __init__(self,tones):
@@ -89,15 +86,13 @@ class Chord(object):
         note_string_ints = self.combine_tones()
         return convert_to_bytes(note_string_ints)
 
-
 class Tone(object):
-    note_dict = {'a':0,'b':2,'c':3,'d':5,'e':7,'f':8,'g':10}
+    note_dict = {'a':0,'b':2,'c':3,'d':5,'e':7,'f':8,'g':10,'rest':0}
     for note in note_dict.keys():
         note_dict[note + '#'] = note_dict[note] + 1
         note_dict[note + 'b'] = note_dict[note] - 1
-    def __init__(self, name, note_value, instrument=crappy_instrument):
-        '''overtone_strengths_dict is a dictionary of the relative initial amplitudes of the fundamental and its overtones
-        overtone_decay_dict is a dict of the time constants of the fundamental and its overtones'''
+    def __init__(self, name, note_value, instrument):
+        ''''''
         self.name = name
         self.note_value = note_value
         self.fund_freq = self.parse_freq()
@@ -108,15 +103,20 @@ class Tone(object):
         self.instrument = instrument
 
     def parse_duration(self):
-        duration = self.note_value*4*60/float(tempo)
+        duration = self.note_value*60/float(tempo)
         return duration
 
     def parse_freq(self):
+        if self.name == 'rest':
+            return 0
         half_steps_above_440 = Tone.note_dict[self.name[0]]+12*self.name[1]
         fund_freq = 440*2**(float(half_steps_above_440)/12)
         return fund_freq
     def convert_to_bytes(self):
         return self.instrument.convert_to_bytes(self.fund_freq,self.duration)
+
+# class Rest(Tone):
+#     def convert_to_bytes(self):
 
 
 class Sine_wave(object):
@@ -140,41 +140,38 @@ class Sine_wave(object):
         bytes = convert_to_bytes(ints)
         return bytes        
 
+def exponential_decay(k,time):
+    return math.exp(-k*time)
+
+def linear_decay(k,time):
+    return (1 - k*time)
+
+def no_decay(k,time):
+    return 1
+
 def build_sine_wave(freq, duration, time_constant=0.0, max_amplitude=wave_peak, decay_function=exponential_decay):
     note_string_ints = []
+    if freq == 0:
+        return [0 for i in range(int(round(sample_rate*duration)))]
     for i in range(0,int(duration*freq*sample_rate/freq)):
         offset = (2**bit - 1)/2.0
         phase = math.pi*2*i*freq/sample_rate
         note_string_ints.append(int(round(max_amplitude*decay_function(time_constant,i/sample_rate)*math.sin(phase)+offset)))
     return note_string_ints
 
-
-
-
 def convert_to_bytes(note_string_ints):
     l = [struct.pack('I',num) for num in note_string_ints]
     bytes  = ''.join(l)
     return bytes
 
-
 def combine_n_waves(*waves):
     note_string_ints = [sum(tup)/float(len(waves)) for tup in zip(*waves)]
     return note_string_ints
-def play(list_of_things):
-    bytes = combine_things(list_of_things)
-    p = subprocess.Popen(['sox', '-r', str(sample_rate), '-b', str(bit) , '-c', '1', '-t', 'raw', '-e', 'unsigned-integer', '-', '-d'], stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-    return p.stdin.write(bytes)
 
 def play2(list_of_ints):
     bytes = convert_to_bytes(list_of_ints)
     p = subprocess.Popen(['sox', '-r', str(sample_rate), '-b', str(bit) , '-c', '1', '-t', 'raw', '-e', 'unsigned-integer', '-', '-d'], stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     return p.stdin.write(bytes)
-
-
-def combine_things(list_of_things):
-    """
-    >>> combine_byte([chord1.to_bytes(),chord2.to_bytes])"""
-    return ''.join([thing.convert_to_bytes() for thing in list_of_things])
 
 def combine_things_ints(list_of_things):
     l = []
@@ -183,66 +180,17 @@ def combine_things_ints(list_of_things):
     return l
 
 
-if __name__ == '__main__':
-    t_0 = time.time()
-    tune1 = bach_herzliebster()
-    t_1 = time.time()
-    print t_1 - t_0
-    play2(tune1)
-    
-    
 
-    
-    # play(chord1.bytes)
-    # play(chord1.bytes)
-    # play(chord1.bytes)
-    # play(chord2.bytes)
-    # play(chord3.bytes)
-    # play(chord3.bytes)
-    # play(chord4.bytes)
-    # play(chord5.bytes)
-    # play(chord5.bytes)
-    # play(chord6.bytes)
-    # play(chord6.bytes)
-    # play(chord7.bytes)
-
-    # play(tone1.bytes)
-    # play(tone1.bytes)
-    # play(tone1.bytes)
-    # play(tone2.bytes)
-    # play(tone3.bytes)
-    # play(tone3.bytes)
-    # play(tone4.bytes)
-    # play(tone5.bytes)
-    # play(tone5.bytes)
-    # play(tone6.bytes)
-    # play(tone6.bytes)
-    # play(tone7.bytes)
-
-    # wave = build_sine_wave(440,0.1)
-
-    # decayed = decayed_sine_wave(wave,1.0)
-    # y = decayed
-    # x = range(len(y))
-    # mpl.scatter(x,y)
-    # mpl.show()
-    # assert combine_two_sine_waves(build_sine_wave(400,2), build_sine_wave(800,2)) == combine_n_waves(build_sine_wave(400,2),build_sine_wave(800,2))
-    
-    # p = subprocess.Popen(['sox', '-r', str(sample_rate), '-b', str(bit) , '-c', '1', '-t', 'raw', '-e', 'unsigned-integer', '-', '-d'], stdin=subprocess.PIPE)
-
-    # # p.stdin.write(make_tone_fundamental_four_overtones(440,2))
-    # p.stdin.write(make_sine_wave_sound(440,2,3.0))
+# def combine_things(list_of_things):
+#     """
+#     >>> combine_byte([chord1.to_bytes(),chord2.to_bytes])"""
+#     return ''.join([thing.convert_to_bytes() for thing in list_of_things])
 
 
-
-
-# def make_sine_wave_sound(freq,duration,time_constant=0,max_amplitude=wave_peak):
-#     ints = build_sine_wave(freq,duration,time_constant)
-#     bytes = make_bytes_wave(ints)
-#     return bytes
-
-
-
+# def play(list_of_things):
+#     bytes = combine_things(list_of_things)
+#     p = subprocess.Popen(['sox', '-r', str(sample_rate), '-b', str(bit) , '-c', '1', '-t', 'raw', '-e', 'unsigned-integer', '-', '-d'], stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+#     return p.stdin.write(bytes)
 
 
 
--- test_music.py
+++ test_music.py
@@ -3,27 +3,45 @@ import make_sine_wave as msw
 import time
 
 
+
+
+dict1 = {1:0.5, 2: 0.1, 3: 0.1, 4: 0.05, 5: 0.05, 6: 0.01, 7:0.01, 8: 0.01, 9: 0.01}
+dict2 = {1: 0.1, 2: 0.2, 3: 0.3, 4: 0.2, 5: 7.0, 6: 12.0, 7: 12.0, 8:7.0, 9: 8.0}
+dict3 = {}
+for i in range(1,5):
+    dict3[i] = msw.linear_decay
+for i in range(5,14):
+    dict3[i] = msw.exponential_decay
+
+crappy_instrument = msw.Instrument('crappy',dict3,dict1,dict2)
+
+dict4 = {}
+for key in dict1.keys():
+    dict4[key]=0
+
+
+silence = msw.Instrument('silent',dict3,dict4,dict2)
  
 def twinkle_twinkle():
 
-    a = msw.Tone(('a',0),1/4.0)
-    e = msw.Tone(('e',0),1/4.0)
-    fsharp = msw.Tone(('f#',0),1/4.0)
-    d = msw.Tone(('d',0),1/4.0)
-    csharp = msw.Tone(('c#',0),1/4.0)
-    csharp_16th = msw.Tone(('c#',0),1/16.0) 
-    b = msw.Tone(('b',0),3/16.0)
+    a = msw.Tone(('a',0),1.0)
+    e = msw.Tone(('e',0),1.0)
+    fsharp = msw.Tone(('f#',0),1.0)
+    d = msw.Tone(('d',0),1.0)
+    csharp = msw.Tone(('c#',0),1.0)
+    csharp_16th = msw.Tone(('c#',0),1/4.0) 
+    b = msw.Tone(('b',0),3/4.0)
     a_whole = msw.Tone(('a',0),1.0/2.0)
-    bass_a = msw.Tone(('a',-2),1.0/4.0)
-    middle_a = msw.Tone(('a',-1),1.0/4.0)
-    bass_csharp = msw.Tone(('c#',-1),1/4.0)
-    middle_d = msw.Tone(('d',-1),1/4.0)
-    bass_b = msw.Tone(('b',-1),1/4.0)
-    bass_gsharp = msw.Tone(('g#',-2),1/4.0)
-    bass_fsharp = msw.Tone(('f#',-2),1/4.0)
-    bass_d = msw.Tone(('d',-2),1/4.0)
-    bass_e = msw.Tone(('e',-2),1/4.0)
-    bass_a_whole = msw.Tone(('a',-2),1/2.0)
+    bass_a = msw.Tone(('a',-2),1.0)
+    middle_a = msw.Tone(('a',-1),1.0)
+    bass_csharp = msw.Tone(('c#',-1),1.0)
+    middle_d = msw.Tone(('d',-1),1.0)
+    bass_b = msw.Tone(('b',-1),1.0)
+    bass_gsharp = msw.Tone(('g#',-2),1.0)
+    bass_fsharp = msw.Tone(('f#',-2),1.0)
+    bass_d = msw.Tone(('d',-2),1.0)
+    bass_e = msw.Tone(('e',-2),1.0)
+    bass_a_whole = msw.Tone(('a',-2),2.0)
     a_16th = msw.Tone(('a',0),1/16.0)
 
     melody = [a,a,e,e,fsharp,fsharp,e,e,
@@ -44,29 +62,29 @@ def twinkle_twinkle():
     return chords
 
 def somewhere():
-    one = msw.Tone(('a',-1),1/2.0)
-    two = msw.Tone(('a',-0),1/2.0)
-    three = msw.Tone(('g#',-1),1/4.0)
-    four = msw.Tone(('e',-1),1/8.0)
-    five = msw.Tone(('f#',-1),1/8.0)
+    one = msw.Tone(('a',-1),2.0,crappy_instrument)
+    two = msw.Tone(('a',-0),2.0,crappy_instrument)
+    three = msw.Tone(('g#',-1),1.0,crappy_instrument)
+    four = msw.Tone(('e',-1),1/2.0,crappy_instrument)
+    five = msw.Tone(('f#',-1),1/2.0,crappy_instrument)
     six = three
-    seven = msw.Tone(('a',-0),1/4.0)
-    eight = msw.Tone(('f#',-2),1/2.0)
-    nine = msw.Tone(('f#',-1),1/2.0)
-    ten = msw.Tone(('e',-1),1.0)
-    offset_bass = msw.Tone(('a',1),2.0,instrument=silence)
+    seven = msw.Tone(('a',-0),1.0,crappy_instrument)
+    eight = msw.Tone(('f#',-2),2.0,crappy_instrument)
+    nine = msw.Tone(('f#',-1),2.0,crappy_instrument)
+    ten = msw.Tone(('e',-1),1.0,crappy_instrument)
+    offset_bass = msw.Tone('rest',2.0,crappy_instrument)
 
-    offset = msw.Tone(('a',1),2.0,instrument=silence)
-    one_bass = msw.Tone(('a',0),1/2.0)
-    two_bass = msw.Tone(('a',1),1/2.0)
-    three_bass = msw.Tone(('g#',0),1/4.0)
-    four_bass = msw.Tone(('e',0),1/8.0)
-    five_bass = msw.Tone(('f#',0),1/8.0)
+    offset = msw.Tone('rest',2.0,crappy_instrument)
+    one_bass = msw.Tone(('a',0),2.0,crappy_instrument)
+    two_bass = msw.Tone(('a',1),2.0,crappy_instrument)
+    three_bass = msw.Tone(('g#',0),1.0,crappy_instrument)
+    four_bass = msw.Tone(('e',0),1/2.0,crappy_instrument)
+    five_bass = msw.Tone(('f#',0),1/2.0,crappy_instrument)
     six_bass = three_bass
-    seven_bass = msw.Tone(('a',1),1/4.0)
-    eight_bass = msw.Tone(('f#',-1),1/2.0)
-    nine_bass = msw.Tone(('f#',0),1/2.0)
-    ten_bass = msw.Tone(('e',0),1.0)
+    seven_bass = msw.Tone(('a',1),1.0,crappy_instrument)
+    eight_bass = msw.Tone(('f#',-1),2.0,crappy_instrument)
+    nine_bass = msw.Tone(('f#',0),2.0,crappy_instrument)
+    ten_bass = msw.Tone(('e',0),1.0,crappy_instrument)
     melody_bass = [one,two,three,four,five,six,seven,eight,nine,ten,offset_bass]
     melody_treble = [offset,one_bass,two_bass,three_bass,four_bass,five_bass,six_bass,seven_bass,eight_bass,nine_bass,ten_bass]
 
@@ -78,15 +96,16 @@ def somewhere():
 
 def bach_herzliebster():
 
-    soprano = msw.Line([[('a',0),1/4.0],[('a',-0),1.0/4.0],[('a',-0),1.0/4.0],[('g#',-1),1.0/4.0],
-                        [('e',-1),1/4.0]],msw.crappy_instrument) 
+    soprano = msw.Line([[('a',0),1.0],[('a',-0),1.0],[('a',-0),1.0],[('g#',-1),1/2.0],[('f#',-1),1/2.0],
+                        [('e',-1),1.0]],crappy_instrument) 
 
-    alto = msw.Line([[('e',-1),1/4.0],[('f',-1),1/4.0],[('f',-1),1/4.0],[('e',-1),1/4.0],[('b',-1),1/4.0]],msw.crappy_instrument)
+    alto = msw.Line([[('e',-1),1.0],[('f',-1),1.0],[('f',-1),1.0],[('e',-1),1.0],[('b',-1),1.0]],crappy_instrument)
     
-    tenor = msw.Line([[('c',-1),1/4.0],[('d',-1),1/4.0],[('c',-1),1/4.0],[('b',-1),1/8.0],[('a',-1),1/8.0],[('g#',-2),1/4.0]],msw.crappy_instrument)
+    tenor = msw.Line([[('c',-1),1.0],[('d',-1),1.0],[('c',-1),1.0],[('b',-1),1/2.0],[('a',-1),1/2.0],
+                    [('g#',-2),1.0]],crappy_instrument)
 
-    bass = msw.Line([[('a',-1),1.0/4.0],[('d',-2),1.0/8.0],[('e',-2),1.0/8.0],[('f',-2),1.0/8.0],[('d',-2),1.0/8.0],[('e',-2),3.0/8.0],
-                    [('d',-2),1.0/8.0]],msw.crappy_instrument)
+    bass = msw.Line([[('a',-1),1.0],[('d',-2),1/2.0],[('e',-2),1/2.0],[('f',-2),1/2.0],[('d',-2),1/2.0],
+                    [('e',-2),3/2.0],[('d',-2),1/2.0]],crappy_instrument)
 
     lines = [soprano,alto,tenor,bass]
     lines_ready = [msw.combine_things_ints(line.make_tones()) for line in lines]
@@ -94,9 +113,36 @@ def bach_herzliebster():
     chords = msw.combine_n_waves(lines_ready[0],lines_ready[1],lines_ready[2],lines_ready[3])
     return chords
 
+def somewhere2():
+    line1 = msw.Line([[('eb',-1),2.0],[('eb',0),2.0],[('d',0),1.0],[('bb',0),1/2.0],[('c',0),1/2.0],[('d',0),1.0],
+                    [('eb',0),1.0],[('eb',-1),2.0],[('c',0),2.0],[('bb',0),4.0],
+                    [('c',-1),2.0],[('ab',-0),2.0],[('g',-1),1.0],[('eb',-1),1/2.0],[('f',-1),1/2.0],
+                    [('g',-1),1.0],[('ab',-0),1.0],[('f',-1),1.0],[('d',-1),0.5],[('eb',-1),0.5],[('f',-1),1.0],
+                    [('g',-1),1.0],[('eb',-1),4.0]],crappy_instrument)
+    line2 = msw.Line([[('bb',-1),2.0],[('bb',0),2.0],[('f',-1),3.0],[('g',-1),1.0],[('eb',-1),4.0],[('eb',-1),4.0],
+                    [('c',-1),3.0],[('d',-1),0.5],[('c',-1),0.5],[('b',-1),2.0],[('c',-1),2.0],[('rest'),1.5],[('eb',-1),1.5],
+                    [('d',-1),1.0],[('eb',-1),4.0]],crappy_instrument)
+    line3 = msw.Line([[('g',-2),2.0],[('g',-1),2.0],[('rest'),4.0],[('c',-1),1.0],[('ab',-1),0.5],[('bb',-1),0.5],
+                    [('c',-1),1.0],[('d',-1),1.0],[('eb',-1),4.0],[('rest'),6.0],[('bb',-1),2.0],[('c',-1),3.0],
+                    [('bb',-1),1.0],[('g',-2),4.0]],crappy_instrument)
+
+    line4 = msw.Line([[('rest'),4.0],[('bb',-2),2.0],[('bb',-1),2.0],[('rest'),1.0],[('ab',-1),3.0],
+                    [('g',-2),1.0],[('eb',-2),0.5],[('f',-2),0.5],[('g',-2),1.0],[('ab',-1),1/2.0],[('bb',-1),0.5],
+                    [('ab',-1),2.0],[('f',-3),2.0],[('g',-3),1.0],[('f',-2),1.0],[('e',-2),1.0],
+                    [('f',-2),0.5],[('g',-2),0.5],[('ab',-1),1.5],[('g',-2),0.5],[('ab',-1),0.5],
+                    [('a',-1),0.5],[('bb',-1),1.0],[('eb',-2),4.0]],crappy_instrument)
+
+    lines = [line1,line2,line3,line4]
+
+    lines_ready = [msw.combine_things_ints(line.make_tones()) for line in lines]
+
+    chords = msw.combine_n_waves(lines_ready[0],lines_ready[1],lines_ready[2],lines_ready[3])
+    return chords
+
 if __name__ == '__main__':
     t_0 = time.time()
-    tune1 = bach_herzliebster()
+    #tune1 = bach_herzliebster()
+    tune1 = somewhere2()
     t_1 = time.time()
     print t_1 - t_0
     msw.play2(tune1)
