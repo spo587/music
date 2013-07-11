edd902ffb310e08ba99ceb3553511da38c5e43ee
spo587 <samueljpost@gmail.com>
2013-07-11 12:04:54 -0400

    cosmetic changes

--- test_music.py
+++ test_music.py
@@ -13,7 +13,14 @@ for i in range(1,5):
 for i in range(5,14):
     dict3[i] = msw.exponential_decay
 
+# dict4 = {1:0.7,}
+# dict5 = 
+# dict6 = 
+
+
 crappy_instrument = msw.Instrument('crappy',dict3,dict1,dict2)
+
+# second_instrument = msw.Instrument()
 
 dict4 = {}
 for key in dict1.keys():
@@ -24,25 +31,25 @@ silence = msw.Instrument('silent',dict3,dict4,dict2)
  
 def twinkle_twinkle():
 
-    a = msw.Tone('a',0,1.0)
-    e = msw.Tone('e',0,1.0)
-    fsharp = msw.Tone('f#',0,1.0)
-    d = msw.Tone('d',0,1.0)
-    csharp = msw.Tone('c#',0,1.0)
-    csharp_16th = msw.Tone('c#',0,1/4.0) 
-    b = msw.Tone('b',0,3/4.0)
-    a_whole = msw.Tone('a',0,1.0/2.0)
-    bass_a = msw.Tone('a',-2,1.0)
-    middle_a = msw.Tone('a',-1,1.0)
-    bass_csharp = msw.Tone('c#',-1,1.0)
-    middle_d = msw.Tone('d',-1,1.0)
-    bass_b = msw.Tone('b',-1,1.0)
-    bass_gsharp = msw.Tone('g#',-2,1.0)
-    bass_fsharp = msw.Tone('f#',-2,1.0)
-    bass_d = msw.Tone('d',-2,1.0)
-    bass_e = msw.Tone('e',-2,1.0)
-    bass_a_whole = msw.Tone('a',-2,2.0)
-    a_16th = msw.Tone('a',0,1/16.0)
+    a = msw.Tone('a',0,1.0,crappy_instrument)
+    e = msw.Tone('e',0,1.0,crappy_instrument)
+    fsharp = msw.Tone('f#',0,1.0,crappy_instrument)
+    d = msw.Tone('d',0,1.0,crappy_instrument)
+    csharp = msw.Tone('c#',0,1.0,crappy_instrument)
+    csharp_16th = msw.Tone('c#',0,1/4.0,crappy_instrument) 
+    b = msw.Tone('b',0,3/4.0,crappy_instrument)
+    a_whole = msw.Tone('a',0,2.0,crappy_instrument)
+    bass_a = msw.Tone('a',-2,1.0,crappy_instrument)
+    middle_a = msw.Tone('a',-1,1.0,crappy_instrument)
+    bass_csharp = msw.Tone('c#',-1,1.0,crappy_instrument)
+    middle_d = msw.Tone('d',-1,1.0,crappy_instrument)
+    bass_b = msw.Tone('b',-1,1.0,crappy_instrument)
+    bass_gsharp = msw.Tone('g#',-2,1.0,crappy_instrument)
+    bass_fsharp = msw.Tone('f#',-2,1.0,crappy_instrument)
+    bass_d = msw.Tone('d',-2,1.0,crappy_instrument)
+    bass_e = msw.Tone('e',-2,1.0,crappy_instrument)
+    bass_a_whole = msw.Tone('a',-2,4.0,crappy_instrument)
+    a_16th = msw.Tone('a',0,1/16.0,crappy_instrument)
 
     melody = [a,a,e,e,fsharp,fsharp,e,e,
               d,d,csharp,csharp,b,a_16th,b,csharp_16th,a_whole]
@@ -71,10 +78,10 @@ def somewhere1():
     seven = msw.Tone('a',-0,1.0,crappy_instrument)
     eight = msw.Tone('f#',-2,2.0,crappy_instrument)
     nine = msw.Tone('f#',-1,2.0,crappy_instrument)
-    ten = msw.Tone('e',-1,1.0,crappy_instrument)
-    offset_bass = msw.Tone('rest',None,2.0,crappy_instrument)
+    ten = msw.Tone('e',-1,4.0,crappy_instrument)
+    offset_bass = msw.Tone('rest',None,8.0,crappy_instrument)
 
-    offset = msw.Tone('rest',None,2.0,crappy_instrument)
+    offset = msw.Tone('rest',None,8.0,crappy_instrument)
     one_bass = msw.Tone('a',0,2.0,crappy_instrument)
     two_bass = msw.Tone('a',1,2.0,crappy_instrument)
     three_bass = msw.Tone('g#',0,1.0,crappy_instrument)
@@ -94,24 +101,6 @@ def somewhere1():
 
     return chords
 
-def bach_herzliebster():
-
-    soprano = msw.Line([['a',0,1.0],['a',-0,1.0],['a',-0,1.0],['g#',-1,1/2.0],['f#',-1,1/2.0],
-                        ['e',-1,1.0]],crappy_instrument) 
-
-    alto = msw.Line([['e',-1,1.0],['f',-1,1.0],['f',-1,1.0],['e',-1,1.0],['b',-1,1.0]],crappy_instrument)
-    
-    tenor = msw.Line([['c',-1,1.0],['d',-1,1.0],['c',-1,1.0],['b',-1,1/2.0],['a',-1,1/2.0],
-                    ['g#',-2,1.0]],crappy_instrument)
-
-    bass = msw.Line([['a',-1,1.0],['d',-2,1/2.0],['e',-2,1/2.0],['f',-2,1/2.0],['d',-2,1/2.0],
-                    ['e',-2,3/2.0],['d',-2,1/2.0]],crappy_instrument)
-
-    lines = [soprano,alto,tenor,bass]
-    lines_ready = [msw.combine_things_ints(line.make_tones()) for line in lines]
-
-    chords = msw.combine_n_waves(lines_ready[0],lines_ready[1],lines_ready[2],lines_ready[3])
-    return chords
 
 def somewhere2():
     line1 = msw.Line([['eb',-1,2.0],['eb',0,2.0],['d',0,1.0],['bb',0,1/2.0],['c',0,1/2.0],['d',0,1.0],
@@ -157,9 +146,29 @@ if __name__ == '__main__':
     tune1 = somewhere2()
     t_1 = time.time()
     print t_1 - t_0
+    #msw.write_to_file(tune1,'ohshit')
     msw.play2(tune1)
     
 
 
+
+def bach_herzliebster():
+
+    soprano = msw.Line([['a',0,1.0],['a',-0,1.0],['a',-0,1.0],['g#',-1,1/2.0],['f#',-1,1/2.0],
+                        ['e',-1,1.0]],crappy_instrument) 
+
+    alto = msw.Line([['e',-1,1.0],['f',-1,1.0],['f',-1,1.0],['e',-1,1.0],['b',-1,1.0]],crappy_instrument)
+    
+    tenor = msw.Line([['c',-1,1.0],['d',-1,1.0],['c',-1,1.0],['b',-1,1/2.0],['a',-1,1/2.0],
+                    ['g#',-2,1.0]],crappy_instrument)
+
+    bass = msw.Line([['a',-1,1.0],['d',-2,1/2.0],['e',-2,1/2.0],['f',-2,1/2.0],['d',-2,1/2.0],
+                    ['e',-2,3/2.0],['d',-2,1/2.0]],crappy_instrument)
+
+    lines = [soprano,alto,tenor,bass]
+    lines_ready = [msw.combine_things_ints(line.make_tones()) for line in lines]
+
+    chords = msw.combine_n_waves(lines_ready[0],lines_ready[1],lines_ready[2],lines_ready[3])
+    return chords
     
     
